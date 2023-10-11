88f758055a39e208998cfdb9ba31f32558dd0504
iris <122833995+irisdaniaj@users.noreply.github.com>
2023-10-11 20:44:19 +0200

    Update train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 
 # Train the model
-results = model.train(data='VisDrone.yaml', epochs=100, imgsz=640)
+results = model.train(data='VisDrone.yaml', epochs=5, imgsz=640) #since the goal of this project was not to have the best perfoming model and since the dataset was very heavy we only trained for five epochs, but feel free to modify the numbers of epochs
